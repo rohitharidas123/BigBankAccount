68946cd5cce67c88498389dcc9e14fdfd9c1d400
rohitharidas123 <42977203+rohitharidas123@users.noreply.github.com>
2019-01-04 13:36:39 -0500

    Add files via upload

---
+++ src/BankAccount.java
@@ -0,0 +1,109 @@
+/**
+ * 
+ * @author Rohit Haridas
+ * Pd 6
+ *AP Comp Sci
+ */
+public abstract class BankAccount 
+{
+	//fields
+	private static int nextAccNum;
+	private String name;
+	private int acctNum;
+	private double balance;
+
+	//constructors
+	/**
+	 * 
+	 * @param n: name of the account holder
+	 */
+	public BankAccount(String n)
+	{
+		acctNum = nextAccNum;
+		nextAccNum++;
+		name = n;
+	}
+	
+	/**
+	 * 
+	 * @param n: name of the account
+	 * @param b: balance in the account
+	 */
+	public BankAccount(String n, double b)
+	{
+		acctNum = nextAccNum;
+		nextAccNum++;
+		name = n;
+		balance = b;
+	}
+	
+	//methods
+	/**
+	 * adds money to balance
+	 * @param amt: amount of money
+	 */
+	public void deposit(double amt)
+	{
+		balance += amt;
+	}
+	
+	/**
+	 * takes out money
+	 * @param amt: amount of money
+	 */
+	public void withdraw(double amt)
+	{
+		balance -= amt;
+	}
+	
+	/**
+	 * 
+	 * @return: returns name of account
+	 */
+	public String getName()
+	{
+		return name;
+	}
+	
+	/**
+	 * 
+	 * @return: returns balance
+	 */
+	public double getBalance()
+	{
+		return balance;
+	}
+	
+	/**
+	 * will be implemented in checkingaccount and savingsaccount classes
+	 */
+	public abstract void endofMonthUpdate();
+	
+	/**
+	 * 
+	 * @param other: represents bank account that will be receiving the transferred money
+	 * @param amt : amount that will be transfered
+	 */
+	public void transfer(BankAccount other, double amt)
+	{
+		balance -= amt;
+		other.deposit(amt);
+	}
+	
+	/**
+	 * prints a bank accounts number, name, and balance
+	 */
+	public String toString()
+	{
+		return (acctNum +  "\t" + name + "\t" + balance);
+	}
+	
+	/**
+	 * 
+	 * @return: returns bank account number
+	 */
+	public int getAcctNum()
+	{
+		return acctNum;
+	}
+}
---
+++ src/SavingsAccount.java
@@ -0,0 +1,109 @@
+
+public class SavingsAccount extends BankAccount
+{
+	//fields
+	private double intRate;
+	private final double MIN_BAL;
+	private final double MIN_BAL_FEE;
+	
+	//constructors
+	/**
+	 * 
+	 * @param n: name of the account holder
+	 * @param b: balance of account
+	 * @param r: interest rate
+	 * @param mb: minimum balance allowed in account
+	 * @param mbf: fee for going below minimum balance
+	 */
+	public SavingsAccount(String n, double b, double r, double mb, double mbf)
+	{
+		super(n, b);
+		intRate = r;
+		MIN_BAL = mb;
+		MIN_BAL_FEE = mbf;
+	}
+	/**
+	 * 
+	 * @param n: name of the account holder
+	 * @param r: interest rate
+	 * @param mb: minimum balance allowed in account
+	 * @param mbf: fee for going below minimum balance
+	 */
+	public SavingsAccount(String n, double r, double mb, double mbf)
+	{
+		this(n, 0, r, mb, mbf);
+	}
+	
+	//methods
+	/**
+	 * deposits money as long as amount is positive
+	 */
+	public void deposit(double amt)
+	{
+		if(amt>=0)
+			super.deposit(amt);
+		else
+			throw new IllegalArgumentException();
+	}
+	
+	/**
+	 * withdraws money as long as amount is positive and withdrawing the money will not result in a negative balance
+	 * if withdrawing money results in balance being less than the minimum balance, fee will be charged
+	 */
+	public void withdraw(double amt)
+	{
+		if(amt>=0)
+		{
+			if(super.getBalance() - amt < 0)
+			{
+				throw new IllegalArgumentException();
+			}
+			else
+			{
+				super.withdraw(amt);
+				if(super.getBalance()< MIN_BAL)
+					super.withdraw(MIN_BAL_FEE);
+			}
+		}
+		else
+			throw new IllegalArgumentException();
+	}
+	
+	/**
+	 * transfers money between two accounts as long as they are under same name
+	 * amount cannot exceed balance of initial account
+	 */
+	public void transfer(BankAccount other, double amt)
+	{
+		if(other.getName().equals(super.getName()))
+		{
+			if(amt>super.getBalance())
+				throw new IllegalArgumentException();
+			else
+			{
+				this.withdraw(amt);
+				other.deposit(amt);
+			}
+		}
+		else
+		{
+			throw new IllegalArgumentException();
+		}
+	}
+	
+	/**
+	 * adds interest to an account
+	 */
+	public void addInterest()
+	{
+		super.deposit(super.getBalance()*intRate);
+	}
+	
+	/**
+	 * adds interest at end of month
+	 */
+	public void endofMonthUpdate()
+	{
+		this.addInterest();
+	}
+}
